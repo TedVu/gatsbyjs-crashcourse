5960319551e006b8aa1063f1839d014cf504d121
tedvu <tedvu184@gmail.com>
2022-02-20 11:40:19 +1100

    Render markdown blog posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,10 +1,39 @@
 import React from "react"
 import Layout from "../components/layout"
+import { graphql, useStaticQuery } from "gatsby"
 export const BlogPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+            html
+            excerpt
+          }
+        }
+      }
+    }
+  `)
+
+  console.log(data.allMarkdownRemark.edges)
   return (
     <Layout>
       <h1>Blog</h1>
-      <p>Posts will show up here later on.</p>{" "}
+      <ol>
+        {data.allMarkdownRemark.edges.map(edge => (
+          <li>
+            <h2>{edge.node.frontmatter.title}</h2>
+            <p>{edge.node.frontmatter.date} </p>
+            <div
+              dangerouslySetInnerHTML={{ __html: `${edge.node.html}` }}
+            ></div>
+          </li>
+        ))}
+      </ol>
     </Layout>
   )
 }
---
+++ src/posts/gatsby.md
@@ -0,0 +1,12 @@
+---
+title: "Gatsby is awesome"
+date: "2022-02-19"
+---
+
+I just launached a new bootcamp
+
+## Topics covered
+
+React
+Gatsby
+GraphQL
